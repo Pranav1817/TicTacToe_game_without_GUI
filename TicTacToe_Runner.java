462f13de4fce7701b2abe72b4aef0eb96b10f47e
Pranav Kumbhalkar <87860944+Pranav1817@users.noreply.github.com>
2022-10-13 20:21:28 +0530

    Add files via upload

---
+++ TicTacToe_Runner.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TicTacToe_Runner {
+
+
+    public static void main(String args[]){
+        TicTacToe_manager t = new TicTacToe_manager();
+        t.startGame();
+    }
+}
